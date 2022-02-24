cc9684d3eae54d886efb3422b1806b7eddcc8abb
Claira J <julia.claira@gmail.com>
2022-02-23 19:52:21 -0800

    completed scrape of flix demographics

--- show_data.py
+++ show_data.py
@@ -3,6 +3,6 @@ import sqlite3
 
 def return_table():
     #set up sqlite
-    connection = sqlite3.connect('flix_shows.db')
-    df=pd.read_sql_query("select * from flix_genz",connection)
+    connection = sqlite3.connect('flix.db')
+    df=pd.read_sql_query("select * from flix_shows",connection)
     return df 
