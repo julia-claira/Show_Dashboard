87e660b9c6385648d09847aa61e4bdd2e7d5c2ec
Claira J <julia.claira@gmail.com>
2022-03-24 10:00:41 -0700

    debug

--- app.py
+++ app.py
@@ -76,9 +76,6 @@ def rt():
 if __name__=="__main__":
     app.run(debug=False)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
 
 
 # In[ ]:
